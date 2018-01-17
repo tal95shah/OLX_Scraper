46d079f193b24a52aab35e5f2df645e522b1141f
Talha Shah <talha95shah@gmail.com>
2018-01-17 18:43:05 +0500

    Add files via upload

---
+++ olx_scraper/args.py
@@ -0,0 +1,9 @@
+def info(string):
+	if string.lower() == 'city':
+		return 'lahore'
+	elif string.lower() == 'product':
+		return 'iphone'
+	elif string.lower() == 'minimumprice':
+		return '25000'
+	else:
+		return ''
